24d95ace0d93ab0c5b98428cc127aefe698eea87
Sumit Poudel <sumitpoudell20s@gmail.com>
2025-12-15 07:44:56 +0545

    add the blog page using the css

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import "./blog.css";
+import { IoPerson } from "react-icons/io5";
 export default function page() {
   return (
-    <div>
-      <h1>welcome to the blog page </h1>
+    <div className="blog-container">
+      <section className="blog-section">
+        <div className="blog-main">
+          <h1 className="blog-header">Blog</h1>
+          <p className="bolg-para">
+            <span className="top-line">
+              Experience the journey from farm to finished product through
+            </span>
+            <span className="bottom-line">
+              captivating visuals of our photography, practices, and production.
+            </span>
+          </p>
+        </div>
+      </section>
+      <section className="blog-featured">
+        <div className="blog-headerproduct">
+          <h1 className="product-header1">Featured Articles</h1>
+        </div>
+        <div className="blog-grid">
+          <div className="blog-card">
+            <div className="blog-image">
+              <img src="./bolgimg/leaf.png" alt="blog" />
+            </div>
+
+            <div className="blog-content">
+              <div className="blog-header1">
+                <div className="blog-meta">
+                  <div className="author">
+                    <div className="author-icon">
+                      <IoPerson size={12} />
+                    </div>
+                    <span>Mr.Shah</span>
+                  </div>
+
+                  <h1 className="blog-date">October 17, 2025</h1>
+                </div>
+
+                <h3 className="blog-title">
+                  Uchha Himal’s Unwavering Commitment to Quality – A Legacy of
+                  Trust and Craftsmanship
+                </h3>
+              </div>
+
+              <div className="blog-footer">
+                <h1>Admin</h1>
+              </div>
+
+              <div className="blog-button">
+                <h1>Learn More</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }

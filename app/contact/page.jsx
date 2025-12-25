ea06f124c61ae53cc3e23a1745e7858a335c5074
Sumit Poudel <sumitpoudell20s@gmail.com>
2025-12-25 23:08:07 +0545

    update the contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -298,7 +298,7 @@ export default function page() {
       </div>
       <section className="py-16 bg-black">
         <div className="container mx-auto px-4">
-          <div className="w-full h-[900px] md:h-[500px] relative rounded-lg overflow-hidden shadow-lg">
+          <div className="w-full h-[300px] md:h-[500px] lg:h-[900px] relative rounded-lg overflow-hidden shadow-lg">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7135.121817549633!2d87.064171!3d26.598462!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef1b001a5f52ad%3A0x6194d7420b1d264a!2sUchha%20Himal%20Product%20Pvt%20Ltd!5e0!3m2!1sen!2snp!4v1765811658234!5m2!1sen!2snp"
               width="100%"
